3fd406983bf5020495d895249da0e412e6fd8169
PatrickInFrontEnd <patryk.pluciennik022@gmail.com>
2019-12-09 21:47:10 +0100

    Refactored code, little bug fixed

--- public/JS/JS Components/pipes_generator.js
+++ public/JS/JS Components/pipes_generator.js
@@ -1,24 +1,82 @@
-export default class Pipe {
+import Pipe from "./Pipe.js";
+
+export default class Pipe_Generator {
     constructor(ctx) {
         this.ctx = ctx;
-        this.width = 100;
-        this.height =
-            (Math.random() * (this.ctx.canvas.height / 2) + 100) %
-            (this.ctx.canvas.height / 2 - 50);
-        if (this.height < 50) this.height = 50;
-        this.x = this.ctx.canvas.width;
-        this.y = 0;
+        this.ch = this.ctx.canvas.height;
+        this.cw = this.ctx.canvas.width;
+        this.generateConstants();
+        this.generateParamsOfPipes();
     }
 
-    drawUpperPipe() {
+    generateConstants = () => {
+        this.__WIDTH_OF_PIPE = 80;
+        this.__HEIGHT = parseInt(
+            (Math.random() * this.ctx.canvas.height + 100) %
+                (this.ctx.canvas.height - 150)
+        );
+        if (this.__HEIGHT < 50) this.__HEIGHT = 50;
+        this.__SPEED = 3;
+    };
+
+    generateParamsOfPipes = () => {
+        this.upperPipe = new Pipe(
+            this.cw,
+            0,
+            this.__WIDTH_OF_PIPE,
+            this.__HEIGHT
+        );
+        const posYBottomPipe = this.__HEIGHT + 150;
+        const heightOfBottomPipe = this.ch - posYBottomPipe;
+
+        this.bottomPipe = new Pipe(
+            this.cw,
+            posYBottomPipe,
+            this.__WIDTH_OF_PIPE,
+            heightOfBottomPipe
+        );
+    };
+
+    drawPipes() {
         this.ctx.fillStyle = "#0f0";
-        this.ctx.fillRect(this.x, this.y, this.width, this.height);
+        this.ctx.fillRect(
+            this.upperPipe.x,
+            this.upperPipe.y,
+            this.upperPipe.w,
+            this.upperPipe.h
+        );
+        this.ctx.fillRect(
+            this.bottomPipe.x,
+            this.bottomPipe.y,
+            this.bottomPipe.w,
+            this.bottomPipe.h
+        );
     }
 
-    updatePipe = () => {
-        this.ctx.clearRect(this.x, this.y, this.width + 1, this.height + 1);
-        this.x -= 3;
-        this.drawUpperPipe();
-        requestAnimationFrame(this.updatePipe);
+    clearPipes = () => {
+        this.ctx.clearRect(
+            this.upperPipe.x,
+            this.upperPipe.y,
+            this.upperPipe.w,
+            this.upperPipe.h
+        );
+
+        this.ctx.clearRect(
+            this.bottomPipe.x,
+            this.bottomPipe.y,
+            this.bottomPipe.w,
+            this.bottomPipe.h
+        );
+    };
+
+    animatePipes = () => {
+        this.upperPipe.x -= this.__SPEED;
+        this.bottomPipe.x -= this.__SPEED;
+    };
+
+    updatePipes = () => {
+        this.clearPipes();
+        this.animatePipes();
+        this.drawPipes();
     };
 }
