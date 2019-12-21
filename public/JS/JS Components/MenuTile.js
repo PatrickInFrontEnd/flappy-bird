7998be8ba0f9a5d224ddd1b297b465b25cd921c7
PatrickInFrontEnd <patryk.pluciennik022@gmail.com>
2019-12-21 22:13:55 +0100

    Little fixed added to the project

--- public/JS/JS Components/MenuTile.js
+++ public/JS/JS Components/MenuTile.js
@@ -8,9 +8,11 @@ class MenuTile extends Vector {
         this.height = height;
     }
 
+    //FIXME: create function that will paint menu tiles from 0 opacity to 1 and from 1 to 0 all tiles together (paint tiles for 0.1 opacity, clearRect etc.)
     showTile = (ctx, i = 0) => {
         if (i < 10) ctx.globalAlpha = `0.0${i}`;
-        else if (i < 100) ctx.globalAlpha = `0.${i}`;
+        else if (i < 100 && i > 10) ctx.globalAlpha = `0.${i}`;
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
         ctx.drawImage(
             this.image,
             0,
@@ -23,14 +25,48 @@ class MenuTile extends Vector {
             this.height
         );
 
-        if (i > 100) {
+        i++;
+        if (i >= 100) {
             ctx.globalAlpha = 1;
+            this.hideTile(ctx, i);
+            console.log(i);
             return;
         }
-        i += 1;
+
         requestAnimationFrame(() => {
             this.showTile(ctx, i);
         });
+    };
+
+    //FIXME: logs 0 infinetely and 100 9 times at showTile
+    hideTile = (ctx, i = 100) => {
+        if (i >= 10) {
+            ctx.globalAlpha = `0.${i}`;
+        } else if (i < 10) {
+            ctx.globalAlpha = `0.0${i}`;
+        }
+        ctx.drawImage(
+            this.image,
+            0,
+            0,
+            this.image.width,
+            this.image.height,
+            this.x,
+            this.y,
+            this.width,
+            this.height
+        );
+        i--;
+        if (i <= 0) {
+            console.log(i);
+            ctx.globalAlpha = 1;
+            return;
+        }
+        if (i > 0) {
+            requestAnimationFrame(() => {
+                this.hideTile(ctx, i);
+            });
+        }
     };
 
     setupCoordinates = (x, y) => {
